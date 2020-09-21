85ed85cb44d93ee1db8cb44221b18c0e73b101ab
yanshouwang <yanshouwang@outlook.com>
2020-09-21 09:04:13 +0800

    原型模式

---
+++ 02. 创建型模式/05. 原型模式/MonkeyKing.cs
@@ -0,0 +1,31 @@
+﻿// 深拷贝和浅拷贝，MemberwiseClone 和 Clone
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace 原型模式
+{
+    class MonkeyKing
+    {
+        public string Name { get; set; }
+
+        public MonkeyKing(string name)
+        {
+            Console.WriteLine("花果山上的一个石头");
+            for (int i = 0; i < 5; i++)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("吸收了 100 年日月精华");
+            }
+            this.Name = name;
+            Console.WriteLine("吃饱了，蹦出来个石猴");
+        }
+
+        public MonkeyKing Copy()
+        {
+            return (MonkeyKing)this.MemberwiseClone();
+        }
+    }
+}
--- 02. 创建型模式/05. 原型模式/Program.cs
+++ 02. 创建型模式/05. 原型模式/Program.cs
@@ -7,6 +7,11 @@ namespace 原型模式
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            MonkeyKing monkey1 = new MonkeyKing("孙悟空");
+            MonkeyKing monkey2 = monkey1.Copy();
+            monkey2.Name = "孙悟空的分身";
+            Console.WriteLine(monkey1.Name);
+            Console.WriteLine(monkey2.Name);
         }
     }
 }
